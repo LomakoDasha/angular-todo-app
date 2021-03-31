2db2826527c6a032d9854bea65fcbd94f6e37590
Lomako Darya <lomakodasha@gmail.com>
2021-03-31 18:30:21 +0300

    component rendered elements testing

--- src/app/components/edit-item-form/edit-item-form.component.spec.ts
+++ src/app/components/edit-item-form/edit-item-form.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule, MatInputModule, MatRadioModule } from '@angular/material';
@@ -25,5 +27,18 @@ describe('EditItemFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should render <h2> element`, () => {
+    const nativeElement: HTMLElement = fixture.nativeElement;
+    const h2Element: HTMLElement = nativeElement.querySelector('.form-title');
+    expect(h2Element.textContent).toEqual('Here you can change selected ToDo item');
+  });
+
+  it(`should render <h2> element with debug element`, () => {
+    const debugElement: DebugElement = fixture.debugElement;
+    const h2Element: DebugElement = debugElement.query(By.css('.form-title'));
+    const h2: HTMLElement = h2Element.nativeElement;
+    expect(h2.textContent).toEqual('Here you can change selected ToDo item');
   });
 });
